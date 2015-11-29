558ccaa686c84b0c78f6a04e1c4d409154f5fc45
Jeff Thompson <jeff@thefirst.org>
2015-11-29 18:52:55 +0100

    Moved readXmlDump from SmwWikidataSync to MediaWiki.

--- csharp/smw-wikidata-sync.cs
+++ csharp/smw-wikidata-sync.cs
@@ -2,41 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
-using System.Xml;
 using System.Text.RegularExpressions;
-using System.Web.Script.Serialization;
 
 namespace Nuvl
 {
   public class SmwWikidataSync
   {
-    public SmwWikidataSync(string gzipFilePath)
-    {
-      using (var file = new FileStream(gzipFilePath, FileMode.Open, FileAccess.Read)) {
-        using (var gzip = new GZipStream(file, CompressionMode.Decompress)) {
-          using (var reader = XmlReader.Create(gzip)) {
-            while (reader.ReadToFollowing("page")) {
-              using (var page = reader.ReadSubtree()) {
-                page.ReadToFollowing("title");
-                var title = page.ReadElementContentAsString();
-                Console.Out.WriteLine("title: " + title);
-
-                // Get the timestamp and text of the last revision.
-                DateTime utcTimeStamp = new DateTime();
-                string text = null;
-                while (page.ReadToFollowing("revision")) {
-                  page.ReadToFollowing("timestamp");
-                  utcTimeStamp = DateTime.Parse(page.ReadElementContentAsString()).ToUniversalTime();
-                  page.ReadToFollowing("text");
-                  text = page.ReadElementContentAsString();
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-
     public static int getWikidataItemId(string wikiText)
     {
       var match = Regex.Match(wikiText, "{{\\w*WikidataItem\\w*\\|(\\d+)}}");
