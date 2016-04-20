75aa35e7d991913492550b93c681c05f519d72ea
Jeff Thompson <jeff@thefirst.org>
2016-04-20 15:33:24 +0200

    Make all the Rule constructors immutable. Make transitiveClosure return Rebindable!(immutable(Rule))[].

--- d/argue.d
+++ d/argue.d
@@ -37,28 +37,48 @@ literalString(string literal) pure
 //let rec subscript x = if x < 0 then "\u208b" + subscript -x else String.map subscriptChar (x.ToString())
 
 // TODO: Move these to a utility file.
-// TODO: Make this generic.
 bool
-isSortedSet(const string[] array) pure
+isSortedSet(T)(const T[] array) pure
 {
   if (!isSorted(array))
     return false;
   
   auto iPrevious = 0;
-  auto i = 1;
-  while (i < array.length) {
+  for (auto i = 1; i < array.length; ++i) {
     if (array[i] == array[iPrevious])
       return false;
-    
+
     iPrevious = i;
-    ++i;
   }
-  
+
   return true;
 }
 
+int
+removeRepeats(T)(T[] array)
+{
+  if (array.length <= 1)
+    // Nothing to do.
+    return array.length;
+
+  auto iPrevious = 0;
+  auto iTo = 1;
+  for (auto iFrom = 1; iFrom < array.length; ++iFrom) {
+    if (array[iFrom] == array[iPrevious])
+      continue;
+
+    if (iTo != iFrom)
+      array[iTo] = array[iFrom];
+
+    iPrevious = iTo;
+    ++iTo;
+  }
+
+  return iTo;
+}
+
 class Rule {
-  this(string consequent, immutable string[] antecedents) pure
+  this(string consequent, immutable string[] antecedents) pure immutable
   {
     this.consequent = consequent;
     if (isSortedSet(antecedents))
@@ -66,6 +86,7 @@ class Rule {
     else {
       auto newAntecedents = antecedents.dup;
       sort(newAntecedents);
+      removeRepeats(newAntecedents);
       // TODO: Remove duplicates.
       this.antecedents = cast(immutable string[])newAntecedents;
     }
@@ -89,11 +110,10 @@ class Rule {
 
     return result ~ " -> " ~ literalString(consequent);
   }
-  
-  override int 
-  opCmp(Object o) pure const
+
+  int
+  opCmp(const Rule other) @safe const pure nothrow
   {
-    auto other = cast(Rule)o;
     if (consequent < other.consequent)
       return -1;
     if (consequent > other.consequent)
@@ -105,17 +125,25 @@ class Rule {
       return 1;
     return 0;
   }
-  
-  Rule[]
+
+  override bool opEquals(Object o) pure
+  {
+    auto other = cast(Rule)o;
+    if (other is null)
+      return false;
+    return consequent == other.consequent && antecedents == other.antecedents; 
+  }
+
+  Rebindable!(immutable(Rule))[]
   transpositions() pure const
   {
-    auto result = new Rule[antecedents.length];
+    auto result = new Rebindable!(immutable(Rule))[antecedents.length];
     for (auto i = 0; i < antecedents.length; ++i) {
       auto newAntecedents = antecedents.dup;
       newAntecedents[i] = neg(consequent);
       sort(newAntecedents);
 
-      result[i] = new Rule(neg(antecedents[i]), cast(immutable string[])newAntecedents);
+      result[i] = new immutable Rule(neg(antecedents[i]), cast(immutable string[])newAntecedents);
     }
 
     sort(result);
@@ -123,14 +151,14 @@ class Rule {
     return result;
   }
   
-  immutable string consequent;
-  immutable string[] antecedents;
+  string consequent;
+  string[] antecedents;
 }
 
-Rule[]
+Rebindable!(immutable(Rule))[]
 transitiveClosure(const Rule[] rules) pure
 {
-  auto result = new Rule[0];
+  auto result = new Rebindable!(immutable(Rule))[0];
   foreach (rule; rules) {
     foreach (x; rule.transpositions())
       result ~= x;
